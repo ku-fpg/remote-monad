02327b7e9db082dcdaa613b342b8581e64b96e95
Andy Gill <andygill@ku.edu>
2016-01-26 13:59:39 -0600

    Testing one single remote monad

--- tests/Test.hs
+++ tests/Test.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE CPP #-}
-{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators #-}
+
 
 {-|
 Module:      Main
@@ -56,46 +58,66 @@ data P :: * -> * where
 
 -- Basic evaluator
 
-runWP :: IORef [A] -> WP.Weak C P a -> IO a
-runWP ref (WP.Command (CommandPush a)) = do
-    print $ "push " ++ show a
+runWP :: IORef [String] -> IORef [A] -> WP.Weak C P a -> IO a
+runWP tr ref (WP.Command (CommandPush a)) = do
+    putStrLn "\n"
     stack <- readIORef ref
     writeIORef ref (a : stack)
+    modifyIORef tr (("push " ++ show a) :)
     return ()
-runWP ref (WP.Procedure (ProcedurePop)) = do
-    print ("pop" :: String)
+runWP tr ref (WP.Procedure (ProcedurePop)) = do
+    modifyIORef tr (("pop") :)
+    putStrLn "\n"
     stack <- readIORef ref
     case stack of
       [] -> return Nothing
       (x:xs) -> do
           writeIORef ref xs
-          print $ "popped " ++ show x
+          modifyIORef tr ((show x) :)
+          putStrLn "\n"
           return (Just x)
 
 
-data DeviceM = Device (IORef [A]) (forall a . M.Remote C P a -> IO a)
+----------------------------------------------------------------
+-- The different ways of running remote monads.
+
+data RemoteMonad = RemoteMonad String (forall a . IORef [String] -> IORef [A] -> M.Remote C P a -> IO a)
+
+instance Show RemoteMonad where
+  show (RemoteMonad msg _) = "Remote Monad: " ++ msg
+  
+instance Arbitrary RemoteMonad where
+  arbitrary = elements 
+    [ runWeakMonadWeakPacket
+    ]
+  
+runWeakMonadWeakPacket :: RemoteMonad
+runWeakMonadWeakPacket = RemoteMonad "WeakMonadWeakPacket" $ \ tr ref -> M.runWeakMonad (runWP tr ref)
+
+----------------------------------------------------------------
+
+data DeviceM = Device (IORef [String]) (IORef [A]) (forall a . M.Remote C P a -> IO a)
 
 sendM :: DeviceM -> M.Remote C P a -> IO a
-sendM (Device _ f) = f
-
-newWeakMonadWeakPacket :: [A] -> IO DeviceM
-newWeakMonadWeakPacket xs = do
-  ref <- newIORef xs
-  return $ Device ref $ M.runWeakMonad (runWP ref)
+sendM (Device _ _ f) = f
 
 newDevice :: [A] 
-          -> (forall a . IORef [A] -> M.Remote C P a -> IO a) 
+          -> RemoteMonad
           -> IO DeviceM
-newDevice xs f = do
+newDevice xs (RemoteMonad _ f) = do
+  tr <- newIORef []
   ref <- newIORef xs
-  return $ Device ref $ f ref
+  return $ Device tr ref $ f tr ref
 
 readDevice :: DeviceM -> IO [A]
-readDevice (Device ref _) = readIORef ref
+readDevice (Device _ ref _) = readIORef ref
 
 cmpDevices :: DeviceM -> DeviceM -> IO Bool
 cmpDevices d1 d2 = (==) <$> readDevice d1 <*> readDevice d2
 
+-- returns backwards, but is for cmp or debugging anyway
+traceDevice :: DeviceM -> IO [String]
+traceDevice (Device tr _ _) = readIORef tr 
 
 ----------------------------------------------------------------
 
@@ -104,20 +126,23 @@ newtype Remote a = Remote (M.Remote C P a)
 instance Show (Remote a) where
   show _ = "<REMOTE>"
 
+instance Arbitrary (Remote A) where
+  arbitrary = sized $ \ n -> Remote <$> arbitraryRemoteMonadA n
+
+----------------------------------------------------------------
+
+data RemoteBind :: * -> * where
+  RemoteBind :: M.Remote C P a -> (a -> M.Remote C P b) -> RemoteBind b
+
 ----------------------------------------------------------------
 
 arbitraryRemoteMonad' :: [Gen (M.Remote C P a)] -> Int -> Gen (M.Remote C P a)
 arbitraryRemoteMonad' base 0 = oneof base 
 arbitraryRemoteMonad' base n = frequency 
   [ (1 , oneof base)
-  , (2 , arbitraryBind (arbitraryRemoteMonad' base) n)
-  ]
-
-arbitraryBind :: (Int -> Gen (M.Remote C P a)) -> Int -> Gen (M.Remote C P a)
-arbitraryBind f n = oneof
-  [ do m <- arbitraryRemoteMonadA (n `div` 2)
-       k  <- promote (`coarbitrary` f (n `div` 2))  -- look for a better way of doing this
-       return $ m >>= k
+  , (1 , do RemoteBind m k <- arbitraryBind (arbitraryRemoteMonad' base) n
+            return (m >>= k)
+    )
   ]
 
 arbitraryRemoteMonadUnit :: Int -> Gen (M.Remote C P ())
@@ -142,36 +167,52 @@ arbitraryRemoteMonadA = arbitraryRemoteMonad'
   ]
 
 
+arbitraryBind :: (Int -> Gen (M.Remote C P a)) -> Int -> Gen (RemoteBind a)
+arbitraryBind f n = oneof
+  [ do m <- arbitraryRemoteMonadUnit (n `div` 2)
+       k  <- promote (`coarbitrary` f (n `div` 2))  -- look for a better way of doing this
+       return $ RemoteBind m k
+  , do m <- arbitraryRemoteMonadMaybeA (n `div` 2)
+       k  <- promote (`coarbitrary` f (n `div` 2))  -- look for a better way of doing this
+       return $ RemoteBind m k
+  , do m <- arbitraryRemoteMonadA (n `div` 2)
+       k  <- promote (`coarbitrary` f (n `div` 2))  -- look for a better way of doing this
+       return $ RemoteBind m k
+  ]
+
 --------------------------------------------------------------------------
 
+-- Check that two remote monad configurations given the same trace and same result
+testRunRemoteMonad :: RemoteMonad -> RemoteMonad -> Remote A -> [A] -> Property
+testRunRemoteMonad runMe1 runMe2 (Remote m) xs = monadicIO $ do
+    dev1 <- run $ newDevice xs runMe2
+    r1 <- run $ sendM dev1 m
+    tr1 <- run $ traceDevice dev1
 
-testRunWeakMonad xs = monadicIO $ do
-    run $ putStrLn $ show xs
-    dev <- run $ newDevice xs (\ ref -> M.runWeakMonad (runWP ref))
-    Remote m <- pick (sized $ \ n -> Remote <$> arbitraryRemoteMonadA n)
-    r <- run $ sendM dev m 
-    return True
+    dev2 <- run $ newDevice xs runMe2
+    r2 <- run $ sendM dev2 m
+    tr2 <- run $ traceDevice dev2
+
+    monitor (collect tr1)
+    return (r1 == r2 && tr1 == tr2)
     
--- Test the remote push action
-prop_push :: [A] -> A -> Property
-prop_push xs x = monadicIO $ do
-    monitor (collect xs)
-    dev <- run $ newDevice xs (\ ref -> M.runWeakMonad (runWP ref) )
+-- Test the remote push primitive
+prop_push :: RemoteMonad -> [A] -> A -> Property
+prop_push runMe xs x = monadicIO $ do
+    dev <- run $ newDevice xs runMe
     () <- run $ sendM dev (M.command (CommandPush x))
     ys <- run $ readDevice  dev
     assert (ys == (x : xs))
 
--- Test the remote pop action 
-prop_pop :: [A] -> Property
-prop_pop xs = monadicIO $ do
-    monitor (collect xs)
-    dev <- run $ newDevice xs (\ ref -> M.runWeakMonad (runWP ref) )
+-- Test the remote pop primitive
+prop_pop :: RemoteMonad -> [A] -> Property
+prop_pop runMe xs = monadicIO $ do
+    dev <- run $ newDevice xs runMe
     r <- run $ sendM dev (M.procedure ProcedurePop)
     ys <- run $ readDevice  dev
     case xs of
       [] -> assert (r == Nothing && ys == [])
       (x':xs') -> assert (r == Just x' && ys == xs')
-
 
 
 --runWeakMonadWeakPacket :: Device -> R.Remote C P a -> IO a
