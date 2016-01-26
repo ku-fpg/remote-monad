2c483da6a22ceeba2f596e71121fa3bb1dfbce1e
Andy Gill <andygill@ku.edu>
2016-01-26 17:23:47 -0600

    Adding runSP and runAppP as native functions

--- tests/Test.hs
+++ tests/Test.hs
@@ -80,10 +80,15 @@ runWP tr ref (WP.Procedure (Pop)) = do
 
 -- TODO: replace with actual packet evaluators (that can call runWP).
 runSP :: IORef [String] -> IORef [A] -> SP.Strong C P a -> IO a
-runSP tr ref = SP.runStrong (runWP tr ref)
+runSP tr ref (SP.Command   c pk) = runWP tr ref (WP.Command c) >> runSP tr ref pk
+runSP tr ref (SP.Procedure p)    = runWP tr ref (WP.Procedure p)
+runSP tr ref (SP.Pure a)         = pure a
 
 runAppP :: IORef [String] -> IORef [A] -> Ap.Remote C P a -> IO a
-runAppP tr ref = Ap.sendApplicative (runWP tr ref)
+runAppP tr ref (Ap.Command   g c) = runAppP tr ref g <*  runWP tr ref (WP.Command c)
+runAppP tr ref (Ap.Procedure g p) = runAppP tr ref g <*> runWP tr ref (WP.Procedure p)
+runAppP tr ref (Ap.Pure a)        = pure a
+
 
 ----------------------------------------------------------------
 -- The different ways of running remote monads.
