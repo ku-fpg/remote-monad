8f29b8642d43228038a03660c4173e5b5239fc1e
Andy Gill <andygill@ku.edu>
2016-01-26 21:41:48 -0600

    Adding the Transport for model of serialization

--- src/Control/Remote/Monad/Packet/Weak.hs
+++ src/Control/Remote/Monad/Packet/Weak.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE StandaloneDeriving #-}
 
 {-|
 Module:      Control.Remote.Monad.Packet.Weak
@@ -12,9 +15,25 @@ Portability: GHC
 
 module Control.Remote.Monad.Packet.Weak where
 
+import Control.Remote.Monad.Packet.Transport 
+
 -- | A Weak Packet, that can encode a command or a procedure.
 
 data Weak (c :: *) (p :: * -> *) (a :: *) where
    Command   :: c   -> Weak c p ()
    Procedure :: p a -> Weak c p a
 
+deriving instance (Show c, Show (p a)) => Show (Weak c p a)
+
+instance Read (Transport (Weak c p)) where
+  readsPrec _ r0 = []
+{-
+        [ (Transport $ Command c p,r2)
+        | (c,r1)      <- reads r0
+        , (";",r2)    <- lex r1
+        , (Transport p,r2) <- reads r2
+        ] ++
+        [ (Transport $ Procedure_ q, r1)
+        | (Transport q,r1) <- reads r0
+        ] 
+ -}
