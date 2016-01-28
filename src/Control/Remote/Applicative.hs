8496a9f7b3b071451b1abda01c7431609d3994d7
Andy Gill <andygill@ku.edu>
2016-01-28 09:05:15 -0600

    Moving instances into DSL, not packet

--- src/Control/Remote/Applicative.hs
+++ src/Control/Remote/Applicative.hs
@@ -31,12 +31,25 @@ module Control.Remote.Applicative
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.State.Strict
 
-import           Control.Remote.Monad.Packet.Applicative
+import           Control.Remote.Monad.Packet.Applicative as A
 import qualified Control.Remote.Monad.Packet.Strong as Strong
 import           Control.Remote.Monad.Packet.Strong (Strong, HStrong(..))
 import qualified Control.Remote.Monad.Packet.Weak as Weak
 import           Control.Remote.Monad.Packet.Weak (Weak)
 import           Control.Natural
+
+instance Functor (RemoteApplicative c p) where
+  fmap f (Command g c)   = Command (fmap f g) c
+  fmap f (Procedure g p) = Procedure (fmap (f .) g) p
+  fmap f (Pure a)        = Pure (f a)
+
+instance Applicative (RemoteApplicative c p) where
+  pure a = Pure a
+  (Pure f) <*> m = fmap f m
+  (Command g c)   <*> (Pure a)        = Command (fmap (\ f -> f a) g) c
+  (Procedure g p) <*> (Pure a)        = Procedure (fmap (\ f a1 -> f a1 a) g) p
+  m <*> (Command g2 c2)               = Command  (m           <*> g2) c2
+  m <*> (Procedure g2 p2)             = Procedure (fmap (.) m <*> g2) p2
 
 -- | promote a command into the applicative
 command :: c -> RemoteApplicative c p ()
--- src/Control/Remote/Monad/Packet/Applicative.hs
+++ src/Control/Remote/Monad/Packet/Applicative.hs
@@ -41,28 +41,7 @@ data RemoteApplicative (c :: *) (p :: * -> *) (a :: *) where
    Command   :: RemoteApplicative c p b        -> c   -> RemoteApplicative c p b
    Procedure :: RemoteApplicative c p (a -> b) -> p a -> RemoteApplicative c p b
    Pure      :: a                                     -> RemoteApplicative c p a 
-
-instance Functor (RemoteApplicative c p) where
-  fmap f (Command g c)   = Command (fmap f g) c
-  fmap f (Procedure g p) = Procedure (fmap (f .) g) p
-  fmap f (Pure a)        = Pure (f a)
-
-instance Applicative (RemoteApplicative c p) where
-  pure a = Pure a
-  (Pure f) <*> m = fmap f m
-  (Command g c)   <*> (Pure a)        = Command (fmap (\ f -> f a) g) c
-  (Procedure g p) <*> (Pure a)        = Procedure (fmap (\ f a1 -> f a1 a) g) p
-  m <*> (Command g2 c2)               = Command  (m           <*> g2) c2
-  m <*> (Procedure g2 p2)             = Procedure (fmap (.) m <*> g2) p2
-
--- | promote a command into the applicative
-command :: c -> RemoteApplicative c p ()
-command c = Command (pure ()) c
-
--- | promote a command into the applicative
-procedure :: p a -> RemoteApplicative c p a
-procedure p = Procedure (pure id) p
-
+ 
 class ApplicativePacket f where
   -- | This overloaded function chooses the best bundling strategy
   --   based on the type of the handler your provide.
