b048500e7ebbebbc53a9315e917b361622c702b3
jtdawso <jdawson@ku.edu>
2016-03-10 20:43:48 -0600

    Moved Promote to Packet.hs

--- src/Control/Remote/Monad.hs
+++ src/Control/Remote/Monad.hs
@@ -25,9 +25,6 @@ module Control.Remote.Monad
   , runStrongMonad
   , runApplicativeMonad
   , runMonadSkeleton
-  , Promote(promote)
-  , promoteToStrong
-  , promoteToApplicative 
   ) where
 
 import Control.Monad.Trans.Class
@@ -146,33 +143,3 @@ runApplicativeMonad (Nat f) = nat $ \ p -> do
     pk (T.Command   c) = A.Command c
     pk (T.Procedure p) = A.Procedure p
     pk (T.Ap g h)      = A.Zip ($) (pk g) (pk h)
-
-
-
-class Promote f where
-    promote :: (Applicative m) => (Weak.WeakPacket c p :~> m) -> (f c p :~> m)
-
-instance Promote A.ApplicativePacket where
-   promote f =  promoteToApplicative f
-
-instance Promote Strong.StrongPacket where
-   promote f = promoteToStrong f
-
-promoteToApplicative :: forall c p m . (Applicative m) => (Weak.WeakPacket c p :~> m) -> (A.ApplicativePacket c p :~> m)
-promoteToApplicative (Nat f) =  Nat $ applicativeFunc
-                    where
-                        applicativeFunc :: (Applicative m) => (A.ApplicativePacket c p a -> m a)
-                        applicativeFunc (A.Command c) =  f (Weak.Command c)
-                        applicativeFunc (A.Procedure p) = f (Weak.Procedure p)
-                        applicativeFunc (A.Zip f1 a b) =  f1 <$> applicativeFunc a <*> applicativeFunc b
-                        applicativeFunc (A.Pure a) = pure a
-
-
-promoteToStrong :: forall c p m . (Applicative m) => (Weak.WeakPacket c p :~> m) -> (Strong.StrongPacket c p :~> m)
-promoteToStrong (Nat f) = Nat $ strongFunc
-                       where 
-                           strongFunc :: (Applicative m) => (Strong.StrongPacket c p a -> m a)
-                           strongFunc (Strong.Command c cmds) = f (Weak.Command c) *> strongFunc cmds
-                           strongFunc (Strong.Procedure p)    = f (Weak.Procedure p)
-                           strongFunc (Strong.Done)           = pure ()
-
