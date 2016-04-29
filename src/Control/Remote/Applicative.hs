59c1a97f925b978db104be4d8aaf1bd75dd036c4
jtdawso <jdawson@ku.edu>
2016-04-29 15:01:41 -0500

    RemoteApplicative ApplicativePacket with Alt

--- src/Control/Remote/Applicative.hs
+++ src/Control/Remote/Applicative.hs
@@ -27,7 +27,8 @@ module Control.Remote.Applicative
   , runWeakApplicative
   , runStrongApplicative
   , runApplicativeApplicative
-  , runAlternativeApplicative
+--  , runAlternativeApplicative
+  , debugApplicative
   ) where
 
 
@@ -65,7 +66,7 @@ local l = T.Local l
 class RunApplicative f where
   -- | This overloaded function chooses the appropriate bundling strategy
   --   based on the type of the handler your provide.
-  runLocalApplicative :: (Monad m) => (l :~> m)->(f c p :~> m) -> (RemoteLocalApplicative l c p:~> m)
+  runLocalApplicative :: (MonadThrow m) => (l :~> m)->(f c p :~> m) -> (RemoteLocalApplicative l c p:~> m)
 
 instance RunApplicative WeakPacket where
   runLocalApplicative = runWeakApplicative
@@ -76,35 +77,30 @@ instance RunApplicative StrongPacket where
 instance RunApplicative ApplicativePacket where
   runLocalApplicative = runApplicativeApplicative
 
-runApplicative :: (RunApplicative f, Monad m) => (f c p :~> m) -> (RemoteApplicative c p :~> m)
+runApplicative :: (RunApplicative f, MonadThrow m) => (f c p :~> m) -> (RemoteApplicative c p :~> m)
 runApplicative = runLocalApplicative $ nat (return . runIdentity)
 
 -- | The weak remote applicative, that sends commands and procedures piecemeal.
-runWeakApplicative :: forall m c p l . (Applicative m) => (l :~> m) -> (WeakPacket c p :~> m) -> (RemoteLocalApplicative l c p :~> m)
-runWeakApplicative (Nat lf) (Nat rf) = nat go 
+runWeakApplicative :: forall m c p l . (MonadThrow m) => (l :~> m) -> (WeakPacket c p :~> m) -> (RemoteLocalApplicative l c p :~> m)
+runWeakApplicative (Nat lf) (Nat rf) = nat $ go  
   where
-    go :: forall a . RemoteLocalApplicative l c p a -> m a
-    go (T.Command   c) = rf (Weak.Command c)
-    go (T.Procedure p) = rf (Weak.Procedure p)
-    go (T.Ap g h)      = go g <*> go h
-    go (T.Pure      a) = pure a
-    go (T.Local     m) = lf m
-    go T.Empty         = throwM RemoteEmptyException
-    go (T.Alt g h)     = do r <- runMaybeT (go3 g <|> go3 h) 
-                            case r of  
-                              Nothing -> throwM RemoteEmptyException
-                              Just a  -> return a
+    go :: forall a . RemoteLocalApplicative l c p a ->  m a
+    go p = do r <- runMaybeT (go2 p) 
+              case r of  
+                Nothing -> throwM RemoteEmptyException
+                Just a  -> return a
+  
+    go2 :: forall a . RemoteLocalApplicative l c p a -> MaybeT m a
+    go2 (T.Command   c)  = lift $ rf (Weak.Command c)
+    go2 (T.Procedure p)  = lift $ rf (Weak.Procedure p)
+    go2 (T.Ap g h)      = go2 g <*> go2 h
+    go2 (T.Pure      a) = pure a
+    go2 (T.Local     m) = lift $ lf m
+    go2 T.Empty         = empty
+    go2 (T.Alt g h)     = (go2 g <|> go2 h) 
                           
-    go3 :: forall a . RemoteApplicative c p a -> (MaybeT m) a
-    go3 (T.Command c)      = lift $ f (Weak.Command c)
-    go3 (T.Procedure p)    = lift $ f (Weak.Procedure p)
-    go3 (T.Ap f g)         = go3 f <*> go3 g
-    go3 (T.Pure a)         = pure a
-    go3 (T.Alt g h)        = go3 g <|> go3 h
-    go3 T.Empty            = empty
-
 -- | The strong remote applicative, that bundles together commands.
-runStrongApplicative :: forall m c p l . (Monad m) => (l :~> m) -> (StrongPacket c p :~> m) -> (RemoteLocalApplicative l c p :~> m)
+runStrongApplicative :: forall m c p l . (MonadThrow m) => (l :~> m) -> (StrongPacket c p :~> m) -> (RemoteLocalApplicative l c p :~> m)
 runStrongApplicative (Nat lf) (Nat rf) = nat $ \ p -> do
     (r,HStrongPacket h) <- runStateT (runMaybeT (go p)) (HStrongPacket id)
     rf $ h $ Strong.Done
@@ -123,34 +119,46 @@ runStrongApplicative (Nat lf) (Nat rf) = nat $ \ p -> do
         r2 <- lift $ rf $ cs $ Strong.Procedure $ p
         return $ r2
     go (T.Ap g h)      = go g <*> go h
-    go (T.Local m)     = lift $ lf m
+    go (T.Local m)     = lift $ lift $ lf m
     go (T.Alt g h)     = go g <|> go h
     go (T.Empty )      = empty
 
 
+
+data Wrapper f a where
+    Value :: f a -> Wrapper f a
+    Throw' :: f () -> Wrapper f a
+
+instance Applicative f => Functor (Wrapper f) where
+    fmap f g = (pure f)<*> g
+
+instance Applicative f => Applicative (Wrapper f) where
+    pure a = Value $ pure a
+    (Value f) <*> (Value g) = Value (f <*> g)
+    (Throw' f) <*> g = Throw' f 
+    (Value f)  <*> (Throw' g) = Throw' (f *> g) 
+
+instance Applicative f => Alternative (Wrapper f) where
+     empty = Throw' (pure ())
+     (Throw' g) <|> (Value h) = Value (g *> h)
+     (Throw' g) <|> (Throw' h) = Throw' (g *> h)
+     (Value g)  <|> _ = Value g
+
+
+
 -- | The applicative remote applicative, that is the identity function.
-runApplicativeApplicative :: forall m c p l . (l :~> m) -> (ApplicativePacket c p :~> m) -> (RemoteLocalApplicative l c p :~> m)
-runApplicativeApplicative (Nat lf) (Nat rf) = nat $ go2
+runApplicativeApplicative :: forall m c p l . (MonadThrow m) => (l :~> m) -> (ApplicativePacket c p :~> m) -> (RemoteLocalApplicative l c p :~> m)
+runApplicativeApplicative (Nat lf) (Nat rf) = nat (go4 . go3)
   where
-    go2 :: forall a . RemoteLocalApplicative l c p a -> m a
-    go2 (T.Local m) =  lf m
-    go2 otherwise = rf  $ go otherwise
+    go3 :: forall a . T.RemoteLocalApplicative l c p a -> Wrapper (ApplicativePacket c p) a
+    go3 (T.Empty)       = empty
+    go3 (T.Pure a)      = pure a
+    go3 (T.Command c)   = Value (A.Command c)
+    go3 (T.Procedure p) = Value (A.Procedure p)
+    go3 (T.Ap g h)      = (go3 g) <*> (go3 h)
+    go3 (T.Alt g h)     = (go3 g) <|> (go3 h)
 
-    go :: forall a . T.RemoteLocalApplicative l c p a -> ApplicativePacket c p a
-    go (T.Pure a)      = A.Pure a
-    go (T.Command   c) = A.Command  c
-    go (T.Procedure p) = A.Procedure p
-    go (T.Ap g h)      = A.Zip ($) (go g) (go h)
---    go (T.Alt g h)     = go g <|> go h
---    go (T.Empty)       = empty
-
-runAlternativeApplicative :: (AlternativePacket c p :~> m) -> (RemoteApplicative c p :~> m)
-runAlternativeApplicative (Nat f) = nat $ f . go
-  where
-    go :: T.RemoteApplicative c p a -> AlternativePacket c p a
-    go (T.Pure a)      = Alt.Pure a
-    go (T.Command   c) = Alt.Command  c
-    go (T.Procedure p) = Alt.Procedure p
-    go (T.Ap g h)      = Alt.Zip ($) (go g) (go h)
-    go (T.Alt g h)     = Alt.Alt (go g)  (go h)
-    go (T.Empty)       = empty
+    go4 :: forall a . Wrapper (ApplicativePacket c p) a -> m a
+    go4 (Value pkt)  = rf pkt
+    go4 (Throw' pkt) = do () <- rf pkt  
+                          throwM RemoteEmptyException   
